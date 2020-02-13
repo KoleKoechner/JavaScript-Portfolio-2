bd40e530ef6361d2cc82e181b1df646bc717bb6b
Kole Koechner <58703962+KoleKoechner@users.noreply.github.com>
2020-02-13 10:01:25 -0600

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,32 @@
+﻿//Tip Calculator
+function calculateTip() {
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
+
+    //Input
+    if (billAmt === "" || serviceQual == 0) {
+        alert("Please enter values");
+        return;
+    }
+    //Check if Input is empty
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    }
+    else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    //Calculate Tip
+    var total = (billAmt * serviceQual) / numOfPeople;
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+
+//Hide Tip Amount
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+document.getElementById("calculate").onclick = function () { calculateTip(); };
